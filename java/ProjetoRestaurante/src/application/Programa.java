1b8936a9197bcdafc299be44c6012408e5ef21db
Rodrigo <119820531+Rodrigo3441@users.noreply.github.com>
2026-05-08 23:01:30 -0300

    application: added comments for some main parts and finished main class

--- java/ProjetoRestaurante/src/application/Programa.java
+++ java/ProjetoRestaurante/src/application/Programa.java
@@ -1,18 +1,39 @@
 package application;
 
 import java.sql.Connection;
+import java.util.Scanner;
 import database.DatabaseConnection;
 import ui.MenuPrincipal;
+import java.util.Locale;
 
-
+/**
+ * Classe: Programa
+ *
+ * Descrição:
+ * Classe principal responsável por apenas iniciar a aplicação
+ *
+ * Responsabilidades:
+ * instanciar a conexão do banco de dados e o scanner usado ao longo de toda a aplicação
+ *
+ * @author Rodrigo
+ * @since 24-04-2026
+ */
 
 public class Programa {
 
 	public static void main(String[] args) {
-
+		
+		//definição dos caracteres para padrão dos Estados Unidos
+		Locale.setDefault(Locale.US);
+		
+		//Conexão com o banco de dados usada ao longo de toda a aplicação
 		Connection conn = DatabaseConnection.getConnection();
 		
-		MenuPrincipal mn = new MenuPrincipal(conn);
+		//Scanner usado ao longo de toda a aplicação
+		Scanner sc = new Scanner(System.in);
+		
+		//Instanciação do menu principal para o início da aplicação
+		MenuPrincipal mn = new MenuPrincipal(conn, sc);
 		mn.mostrar();
 	}
 }
